d667981d6e41305031ac54c92956dc87469ef7ac
Andreas <89839661+Andreasd21@users.noreply.github.com>
2022-09-30 13:45:59 +0200

    Fixed problem with cor
    Was not allowed to start application

--- back-end/SignalRHub/SignalRHub/Models/Employee.cs
+++ back-end/SignalRHub/SignalRHub/Models/Employee.cs
@@ -2,5 +2,10 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+
+        public string ConnectionString { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+
     }
 }
--- back-end/SignalRHub/SignalRHub/Models/Room.cs
+++ back-end/SignalRHub/SignalRHub/Models/Room.cs
@@ -2,5 +2,8 @@
 {
     public class Room
     {
+        public int Id { get; set; }
+        public Employee employee { get; set; }
+        public List<string> EndUserIds { get; set; }
     }
 }
