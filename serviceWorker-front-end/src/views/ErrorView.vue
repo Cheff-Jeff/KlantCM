34599d1106925da59b8e2f00edc0f9ff81f84c1c
TimMuffin9 <tim.denhartigh09@gmail.com>
2022-10-06 09:57:54 +0200

    ErrorView added

---
+++ serviceWorker-front-end/src/views/ErrorView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="mt-5 padtop7 text-center text-danger">Error</h1>
+    <br>
+    <h2 class="text-center">Er is iets fout gegaan!</h2>
+</template>
+
+<style>
+    .padtop7 {
+        padding-top: 7rem;
+    }
+</style>
