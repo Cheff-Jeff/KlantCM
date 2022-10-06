147cce1f7c0480bc78a5ff753c6b5e98b91a57d5
TimMuffin9 <tim.denhartigh09@gmail.com>
2022-10-06 15:17:02 +0200

    added button to error

--- serviceWorker-front-end/src/views/ErrorView.vue
+++ serviceWorker-front-end/src/views/ErrorView.vue
@@ -4,12 +4,29 @@
 
 <template>
     <h1 class="mt-5 padtop7 text-center text-danger">Error</h1>
-    <br>
     <h2 class="text-center">Er is iets fout gegaan!</h2>
+    <div class="buttonspace" v-if="login">
+        <button class="btn btn-danger" @click="loginPage">Login</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            loginPage() {
+                //Navigeer naar test view;
+                this.$router.push("login");
+            }
+        }
+    }
+</script>
 
 <style>
     .padtop7 {
         padding-top: 7rem;
+    }
+
+    .buttonspace {
+        text-align: center;
     }
 </style>
