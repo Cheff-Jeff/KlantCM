d4e0a61b71795cfa6b9ea48a1b6131c21e27b62b
NooitNiets <NooitNietsyt@gmail.com>
2022-10-06 11:11:32 +0200

    kuthoerending

--- back-end/SignalRHub/SignalRTest/testunittest.cs
+++ back-end/SignalRHub/SignalRTest/testunittest.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SignalRHub;
+using SignalRHub.Hubs;
 
 namespace SignalRTest
 {
     internal class testunittest
     {
+        [TestMethod]
+        public void test1()
+        {
+            var hub = new Chathub;
+        }
     }
 }
