62060aa5e20a63b14655823cf109881d8d1f9407
NooitNiets <NooitNietsyt@gmail.com>
2022-10-06 11:06:33 +0200

    wat een getyf

---
+++ back-end/SignalRHub/SignalRtests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SignalRtests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignalRtests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d43ba02-b444-4eb1-96d8-dec2e8ad5ed1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ back-end/SignalRHub/SignalRtests/SignalrTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace SignalRtests
+{
+    [TestClass]
+    public class SignalrTest
+    {
+        [TestMethod]
+        public void StartRoom()
+        {
+
+        }
+    }
+}
