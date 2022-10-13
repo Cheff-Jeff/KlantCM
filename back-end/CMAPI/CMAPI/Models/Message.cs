bea6d913a48cc524dc654f07f24214c19abbe887
NooitNiets <NooitNietsyt@gmail.com>
2022-10-13 11:15:04 +0200

    wel een project die niet leeg is

--- back-end/CMAPI/CMAPI/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CMAPI.Models
-{
-    public class Message
-    {
-        public int Id { get; set; }
-        public string MessageText { get; set; } = string.Empty;
-        public int RoomId { get; set; }
-    }
-}
--- back-end/CMAPI/CMAPI/Models/Room.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CMAPI.Models
-{
-    public class Room
-    {
-        public int Id { get; set; }
-        public string UserId { get; set; } = string.Empty;
-        public bool Rating { get; set; }
-    }
-}
