893c6caefd744c6eaf586f06ea95fd8139c66da0
Mattijs Pronk <456410@student.fontys.nl>
2022-10-13 14:12:05 +0200

    Register/Login added

---
+++ back-end/CM-API-EF/CM-API-EF/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace CM_API_EF.Controllers
+{
+    public class AuthController : Controller
+    {
+        User user = new User();
+        VerifyInfo verifyInfo = new VerifyInfo();
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDTO request)
+        {
+            verifyInfo.CreatePasswordHash(request.Password, out byte[] passwordhash, out byte[] passwordsalt);
+
+            user.userName = request.userName;
+            user.passwordHash = passwordhash;
+            user.passwordSalt = passwordsalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDTO request)
+        {
+            if(user.userName != request.userName)
+            {
+                return BadRequest("No user found");
+            }
+            if(!verifyInfo.VerifyPasswordHash(request.Password, user.passwordHash, user.passwordSalt))
+            {
+                return BadRequest("No user found");
+            }
+            return Ok();
+        }
+    }
+}
