9d636f146adfa7bd0b249bcfe7b5d28cf31cfbef
TimMuffin9 <tim.denhartigh09@gmail.com>
2022-10-13 15:10:00 +0200

    Queue messages added

--- user-front-end/src/assets/javascript/Chat.js
+++ user-front-end/src/assets/javascript/Chat.js
@@ -1,11 +1,12 @@
-import  { HubConnectionBuilder } from "@microsoft/signalr"
+import { HubConnectionBuilder } from "@microsoft/signalr"
 
 export class ChatHub {
-    constructor(){
+    constructor() {
         console.log("in hub")
+
         this.connection = new HubConnectionBuilder().withUrl("https://localhost:44302/signalr").build()
 
-        this.connection.onclose(async () => {
+        this.connection.onclose(async() => {
             await this.connect();
         })
 
@@ -13,43 +14,53 @@ export class ChatHub {
 
 
 
-        this.connection.on("ReceiveMessage", function (message) {
+        this.connection.on("ReceiveMessage", function(message) {
             ///Event to get message 
             ///here you have to imput the message in the chatbox
-            localStorage.setItem('NewChat',message)
+            localStorage.setItem('NewChat', message)
             window.dispatchEvent(NewChat)
         });
 
-        this.connection.on("RecieveRoomId", function (message) {
+        this.connection.on("RecieveRoomId", function(message) {
             ///Event to get message 
             ///here you get the room id
             localStorage.setItem('roomId', message)
+                // clearInterval(QueueTimer)
         });
 
         const NewChat = new Event('NewChat')
     }
-
-    async connect(){
+    async connect() {
         try {
             await this.connection.start();
             console.log("SignalR Connected.");
         } catch (err) {
             console.log(err);
-        }  
+        }
     }
 
-    SendMessage(message){
-        this.connection.invoke("SendMessage", message,localStorage.getItem('roomId'),null).catch(function (err) {
+    SendMessage(message) {
+        this.connection.invoke("SendMessage", message, localStorage.getItem('roomId'), null).catch(function(err) {
             return console.error(err.toString())
         })
     }
 
-    ConnectUser(){
+    ConnectUser() {
         console.log("connect user")
-        this.connection.invoke("ConnectUser").catch(function (err) {
+        this.connection.invoke("ConnectUser").catch(function(err) {
             return console.error(err.toString())
+        })
+
+        let QueueTimer = setInterval(function() {
+            SendQueueMessage()
+        }, 10000);
+
+        function SendQueueMessage() {
+            localStorage.setItem('NewChat', 'We are busy!')
+
+            const NewChat = new Event('NewChat')
+            window.dispatchEvent(NewChat)
         }
-        )
     }
 
 
