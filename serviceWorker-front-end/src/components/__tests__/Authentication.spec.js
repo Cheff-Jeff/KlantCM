1acc372a50ee1bdacffee4e5720d622d9d0bff0d
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-10-20 13:31:28 +0200

    update

--- serviceWorker-front-end/src/components/__tests__/Authentication.spec.js
+++ serviceWorker-front-end/src/components/__tests__/Authentication.spec.js
@@ -35,5 +35,16 @@ describe('RouteGaurd', () => {
 /*
   End unit test for RouteGaurd
 
-  Unit Test RouteGaurd
+  Unit Test Login
 */
+import { Login } from '../../assets/javascript/Authenticate'
+describe('Login', () => {
+  test('User sign in', () => {
+    expect(Login('admin@adminmail.com', 'adminadmin')).toBe(true)
+  })
+})
+describe('Login', () => {
+  test('User sign in faild', () => {
+    expect(Login('fail@mail.com', 'admin')).toBe(false)
+  })
+})
