b0e3e7027dba3f04c2ac68403241b131fdd9de23
NooitNiets <NooitNietsyt@gmail.com>
2022-10-21 09:17:45 +0200

    added unittestproject

---
+++ back-end/SignalRHub/Unittests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using SignalRHub;
+using SignalRHub.Models;
+
+namespace Unittests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
