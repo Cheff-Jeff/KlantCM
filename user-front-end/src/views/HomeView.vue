15d3373e6077f26cd9f384a43cbbeed9f10b200e
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-11-03 14:52:53 +0100

    update toggle review front-end

--- user-front-end/src/views/HomeView.vue
+++ user-front-end/src/views/HomeView.vue
@@ -7,6 +7,9 @@
 
 <template>
   <div class="wraper">
+    <button @click="openRating">
+      open review
+    </button>
     <div class="page-wrap" v-auto-animate>
       <div class="chat-wrap" v-if="modal" :class="modal">
         <svg class="close-cross" @click="toggleModal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M175 175C184.4 165.7 199.6 165.7 208.1 175L255.1 222.1L303 175C312.4 165.7 327.6 165.7 336.1 175C346.3 184.4 346.3 199.6 336.1 208.1L289.9 255.1L336.1 303C346.3 312.4 346.3 327.6 336.1 336.1C327.6 346.3 312.4 346.3 303 336.1L255.1 289.9L208.1 336.1C199.6 346.3 184.4 346.3 175 336.1C165.7 327.6 165.7 312.4 175 303L222.1 255.1L175 208.1C165.7 199.6 165.7 184.4 175 175V175zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
@@ -35,7 +38,7 @@
             <Input @text="sendConverzation"/>
           </div>
         </div>
-        <div class="review-wrapper">
+        <div class="review-wrapper" :class="rating">
           <div class="wrap">
             <div class="text">
               <p>The chat has been closed.</p>
@@ -84,6 +87,7 @@
         opner: '',
         clickedGood: '',
         clickedBad: '',
+        rating: 'hide'
       }
     },
     mounted(){
@@ -99,6 +103,9 @@
       window.addEventListener("resize", this.checkWindow);
     },
     methods: {
+      openRating(){
+        this.rating ='open'
+      },
       ratingGood(){
         this.clickedGood = 'clicked'
       },
