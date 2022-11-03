cf9bb69ee94ea27f3bd8041ea705b2112a55a537
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-11-03 09:24:59 +0100

    update

--- serviceWorker-front-end/src/components/__tests__/Components.spec.js
+++ serviceWorker-front-end/src/components/__tests__/Components.spec.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 /*
   Test Login form component mount
 */
@@ -23,5 +23,93 @@ describe('login form email validation', () => {
 
     expect(wrapper.vm.validateEmail(testMail)).toBe(true)
     expect(wrapper.vm.validateEmail(badEmail)).toBe(false)
+  })
+})
+/*
+  End login form test
+
+  Test converzation bubbels
+*/
+import ConverzationSend from '../ConverzationSend.vue'
+describe('blue bubble', () => {
+  it('mount properly', () => {
+    const txt  = 'Test text'
+    const time = '12:10'
+    const wrapper = mount(ConverzationSend, { 
+      props: { 
+        Time: time, 
+        Text: txt 
+      } 
+    })
+
+    expect(wrapper.text()).toContain(txt)
+    expect(wrapper.text()).toContain(time)
+  })
+})
+
+import ConverzationHelp from '../ConverzationHelp.vue'
+describe('white bubble', () => {
+  it('renders properly', () => {
+    const time = '00:00'
+    const txt  = 'Test converzation.'
+    const wrapper = mount(ConverzationHelp, {
+      props: {
+        Time: time,
+        Text: txt
+      }
+    })
+
+    expect(wrapper.text()).toContain(txt)
+    expect(wrapper.text()).toContain(time)
+  })
+})
+/*
+  End converzation bubbles test
+
+  Test chat input
+*/
+import ChatInput from '../ChatInput.vue'
+describe('validation', () => {
+  it('validates length good', () => {
+    const value = 'test'
+    const wrapper = mount(ChatInput)
+    wrapper.find('#sendBox').setValue(value)
+    wrapper.vm.checkKeyEnter()
+
+    expect(wrapper.vm.inputError).toBe('')
+  })
+})
+
+describe('validation', () => {
+  it('validates length error', () => {
+    const value = 'Test'
+    const wrapper = mount(ChatInput)
+    wrapper.find('#sendBox').setValue(value)
+    wrapper.vm.checkInput()
+
+    expect(wrapper.vm.inputError).toBe('')
+  })
+})
+
+describe('validation', () => {
+  it('validates length error', () => {
+    const value = ''
+    const wrapper = mount(ChatInput)
+    wrapper.find('#sendBox').setValue(value)
+    wrapper.vm.checkInput()
+
+    expect(wrapper.vm.inputError).toBe('Message can not be empty.')
+  })
+})
+
+describe('form emit', () => {
+  it('emits value good', () => {
+    const value = 'test'
+    const wrapper = mount(ChatInput)
+    wrapper.find('#sendBox').setValue(value)
+    wrapper.vm.submit()
+
+    console.log(wrapper.emitted())
+    expect(wrapper.emitted().text[0][0]).toBe(value)
   })
 })
