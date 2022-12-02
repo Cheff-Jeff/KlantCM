ea34cc572fbd9aa4246870a2a934fdbc864a31e4
NooitNiets <NooitNietsyt@gmail.com>
2022-12-02 10:36:13 +0100

    updated

---
+++ back-end/CM-API-EF/CM-API-EF/Business Logic/Statistics.cs
@@ -0,0 +1,77 @@
+﻿using CM_API_EF.Models;
+
+namespace CM_API_EF.Business_Logic
+
+{
+    public class Statistics
+    {
+        public double AverageRating(List<Rating> ratings)
+        {
+            List<int> AverageRating = new List<int>();
+            int factor;
+            double totalRatings;
+            int RateSum;
+            double ratingPercentage;
+            foreach (Rating rating in ratings)
+                if (rating.Rate == true)
+                {
+                    factor = 1;
+                    AverageRating.Add(factor);
+                }
+                else
+                {
+                    factor = 0;
+                    AverageRating.Add(factor);
+                }
+            totalRatings = AverageRating.Count;
+            RateSum = AverageRating.Sum();
+            ratingPercentage = RateSum / totalRatings * 100;
+            return ratingPercentage;
+
+        }
+
+        public double Average(List<int> Indexes)
+        {
+            double total = Indexes.Count;
+            double sum = Indexes.Sum();
+            double percentage = sum / total * 100;
+            return percentage;
+        }
+
+        public double FindMedian(List<int> indexes)
+        {
+            double median = 0;
+            int[] a = indexes.ToArray();
+            Array.Sort(a);
+            int arrayLength = a.Length;
+            int mid = arrayLength / 2;
+            if (arrayLength % 2 == 0)
+            {
+                double value1 = a[mid];
+                double value2 = a[mid - 1];
+                median = (value1 + value2) / 2;
+            }
+            if (arrayLength % 2 == 1)
+            {
+                median = a[mid];
+            }
+
+            return median;
+
+
+        }
+        public TimeSpan ConvertToMinutes(int seconds)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(seconds);
+            return t;
+        }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ back-end/CM-API-EF/CM-API-EF/Controllers/StatisticsController.cs
@@ -0,0 +1,62 @@
+﻿using CM_API_EF.Business_Logic;
+using CM_API_EF.Data;
+using CM_API_EF.Models;
+using CM_API_EF.StatisticModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CM_API_EF.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public readonly UserDbContext _context;
+
+        public StatisticsController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("GetAverageRating")]
+        public async Task<ActionResult> GetAverageRating()
+        {
+            List<Rating> ratingList = await _context.Ratings.ToListAsync();
+            Statistics stats = new Statistics();
+            double percentage = stats.AverageRating(ratingList);
+            percentage = Math.Round(percentage, 2);
+            return Ok(percentage);
+        }
+
+        [HttpGet("GetAverageRatingByUserID")]
+        public async Task<ActionResult> GetAverageRatingByUserID(int userID)
+        {
+            List<Rating> ratingList = (from ra in _context.Ratings
+                                       join ro in _context.Rooms
+                                       on ra.RoomId equals ro.RoomId
+                                       where ro.UserID == userID
+                                       select ra).ToList();
+            Statistics stats = new Statistics();
+            double percentage = stats.AverageRating(ratingList);
+            percentage = Math.Round(percentage, 2);
+            return Ok(percentage);
+        }
+
+        [HttpGet("GetAverageRatingFromLastMonth")]
+        public async Task<ActionResult> GetAverageRatingFromLastMonth()
+        {
+            var firstDay = DateTime.Today.AddDays(-30);
+
+
+            List<Rating> ratingList = await _context.Ratings
+                .Where(r =>
+                r.TimeOfRating >= firstDay)
+                .ToListAsync();
+            Statistics stats = new Statistics();
+            double percentage = stats.AverageRating(ratingList);
+            if (percentage == null || percentage == 0) { return BadRequest("Sumthing went wrung :("); }
+            percentage = Math.Round(percentage, 2);
+            return Ok(percentage);
+        }
+
+    }
+}
