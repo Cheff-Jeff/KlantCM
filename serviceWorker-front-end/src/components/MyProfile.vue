96499e1a97d0ccd02ab9240d0208a9ac800eeaf3
TimMuffin9 <tim.denhartigh09@gmail.com>
2022-12-08 14:52:13 +0100

    Updated AccountView

---
+++ serviceWorker-front-end/src/components/MyProfile.vue
@@ -0,0 +1,51 @@
+<script setup>
+  defineProps({
+    User: {
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <div class="container">
+    <h1>My account</h1>              
+    <div class="color-black">
+      Name: {{user.userName}}
+      <br>
+      Email: {{user.email}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        id: this.User.userId,
+        userName: this.User.userName,
+        email: this.User.email,
+        password: '',
+        isAdmin: this.User.isAdmin
+      },
+      repass: '',
+      nameError: '',
+      emailError: '',
+      idError: '',
+      passwordError: '',
+      matchPassErr: '',
+      submitError: ''
+    }
+  },
+  mounted() {
+    console.log(this.user)
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style lang="scss" >
+@import "../assets/styles/components/MyProfile.scss";
+</style>
--- serviceWorker-front-end/src/views/AccountView.vue
+++ serviceWorker-front-end/src/views/AccountView.vue
@@ -1,8 +1,31 @@
 <script setup>
+import {GetUserById} from '../assets/javascript/User'
 import Header from '../components/Header.vue';
+import MyProfileVue from '../components/MyProfile.vue';
 </script>
 
 <template>
   <Header />
-  <h1 class="mt-5 pt-5">Account</h1>
+  <MyProfileVue v-if="user" :User="user[0]" />
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      user: '',
+      userId: ''
+    }
+  },
+  async mounted() {
+    await this.getUser()
+  },
+  methods: {
+    async getUser() {
+      this.userId = await JSON.parse(sessionStorage.getItem('user'))
+      this.user = await GetUserById(this.userId)
+      console.log( this.user[0])
+    }
+  }
+}
+</script>
