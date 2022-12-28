b24a1c455c549534a32ca4a3ae19f3c924f51081
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-12-28 19:55:37 +0100

    update

--- serviceWorker-front-end/src/components/RatingGraph.vue
+++ serviceWorker-front-end/src/components/RatingGraph.vue
@@ -7,29 +7,43 @@ import { ref } from 'vue';
 const text = ref(null);
   text.value = getLang();
   defineExpose({text})
+  defineProps({
+    id: {
+      required: true,
+    },
+  });
 </script>
 
 
 <template>
-  <div class="container">
-      <h1 class="title">UserStats: {{ userName }}</h1>
-
-      <br/><br/>
-      <div class="custom-progress progress">
-        <div role="progressbar" id="rating" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:" class="animated custom-bar progress-bar slideInLeft bg-orange"></div>
-          </div>
-            <div class="progress-text">
-              <div class="row">
-              <div class="col-7"><p>Average rating</p></div>
-          <div class="col-5 text-right"><p>{{averageRating}}% </p></div>
+  <section class="graph-view">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <span @click="turnBack" class="bread-crum">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg> Back
+          </span>
         </div>
       </div>
-      <br/><br/>
+        <h1 class="title">UserStats: {{ userName }}</h1>
 
-    <div class="chartitem">
-      <canvas id="chart"></canvas>
+        <br/><br/>
+        <div class="custom-progress progress">
+          <div role="progressbar" id="rating" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:" class="animated custom-bar progress-bar slideInLeft bg-orange"></div>
+            </div>
+              <div class="progress-text">
+                <div class="row">
+                <div class="col-7"><p>Average thumbs up rating</p></div>
+            <div class="col-5 text-right"><p>{{averageRating}}% </p></div>
+          </div>
+        </div>
+        <br/><br/>
+
+      <div class="chartitem">
+        <canvas id="chart"></canvas>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 
@@ -44,7 +58,7 @@ export default{
         }
     },
     async mounted(){
-      var userid = sessionStorage.getItem('uDetails')
+      var userid = this.id
 
       this.User = await GetUserById(userid)
       this.userName = this.User[0].userName
@@ -91,21 +105,15 @@ export default{
       .catch(error => {
         console.log(error)
       });
-    }
+    },
+    methods: {
+      turnBack(){
+        this.$emit('switchBack')
+      },
+    },
 }
 </script>
 
-<style scoped>
-.chartitem{
-  width: 100%;
-}
-
-p{
-  color: black;
-  font-size: 20px;
-}
-.container{
-  width: 100%;
-  height: 100%;
-}
+<style lang="scss">
+@import "../assets/styles/components/graph.scss";
 </style>
